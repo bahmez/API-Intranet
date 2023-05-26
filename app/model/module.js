66c06fb2bbe4f120a73e2c565153c3b08293d624
Nabzilooo <adil.nouiri@epitech.eu>
2023-05-26 18:31:50 +0200

    [Feat] (model) : edit module.js

--- app/model/module.js
+++ app/model/module.js
@@ -21,36 +21,184 @@ export async function getModuleInformation(scolarYear, code, codeInstance, cooki
             cookie: cookie
         }
     };
-    const result = await fetch("https://intra.epitech.eu/module/" + scolarYear + "/" + code + "/" + codeInstance, opts)
+    const result = await fetch("https://intra.epitech.eu/module/" + scolarYear + "/" + code + "/" + codeInstance + "/?format=json", opts)
     if (result.statusCode >= 400)
         throw "error code : " + result.statusCode;
-    const dom = new JSDOM(await result.text());
+    return result.json();
+}
 
-    const description = dom.window.document.querySelector(".item.desc .text").innerText;
-    const credit = dom.window.document.querySelector(".item.desc.credits span").innerText;
-
-    const activities = dom.window.document.querySelectorAll("li.activite");
-    const projects = [];
-
-    activities.forEach((value) => {
-        const dates = value.querySelectorAll(".rzone .item.date");
-        const startDate = convertStringToDate(dates[0].querySelector("span:not(.icon)").innerText)
-        const endDate = convertStringToDate(dates[1].querySelector("span:not(.icon)").innerText)
-        const description = value.querySelector(".data .item.description .text").innerText;
-        const dataCode = value.getAttribute("data-code");
-        const title = value.querySelector(".item.title .acti-title").innerText;
-        var type;
-        const typeENUM = ["proj", "tp", "class", "rdv"];
-        const isPast =  value.classList.contains("past");
-
-        for (const typeValue of typeENUM) {
-            if (value.classList.contains(typeValue)) {
-                type = typeValue;
-                break;
-            }
+export async function getProjectInformation(scolarYear, code, codeInstance, codeActivity, cookie) {
+    const opts = {
+        headers: {
+            cookie: cookie
         }
-        const json = {startDate, endDate, description, dataCode, title, type, isPast};
-        projects.push(json);
-    })
-    return {description, credit, projects};
+    };
+    const result = await fetch("https://intra.epitech.eu/module/" + scolarYear + "/" + code + "/" + codeInstance + "/" + codeActivity + "/project/?format=json", opts)
+    if (result.statusCode >= 400)
+        throw "error code : " + result.statusCode;
+    return result.json();
+}
+
+export async function getRdvInformation(scolarYear, code, codeInstance, codeActivity, cookie) {
+    const opts = {
+        headers: {
+            cookie: cookie
+        }
+    };
+    const result = await fetch("https://intra.epitech.eu/module/" + scolarYear + "/" + code + "/" + codeInstance + "/" + codeActivity + "/rdv/?format=json", opts)
+    if (result.statusCode >= 400)
+        throw "error code : " + result.statusCode;
+    return result.json();
+}
+
+export async function getNoteInformation(scolarYear, code, codeInstance, codeActivity, cookie) {
+    const opts = {
+        headers: {
+            cookie: cookie
+        }
+    };
+    const result = await fetch("https://intra.epitech.eu/module/" + scolarYear + "/" + code + "/" + codeInstance + "/" + codeActivity + "/note/?format=json", opts)
+    if (result.statusCode >= 400)
+        throw "error code : " + result.statusCode;
+    return result.json();
+}
+
+export async function getEventInformation(scolarYear, code, codeInstance, codeActivity, codeEvent, cookie) {
+    const opts = {
+        headers: {
+            cookie: cookie
+        }
+    };
+    const result = await fetch("https://intra.epitech.eu/module/" + scolarYear + "/" + code + "/" + codeInstance + "/" + codeActivity + "/" + codeEvent + "/?format=json", opts)
+    if (result.statusCode >= 400)
+        throw "error code : " + result.statusCode;
+    return result.json();
+}
+
+export async function getRegisteredEvent(scolarYear, code, codeInstance, codeActivity, codeEvent, cookie) {
+    const opts = {
+        headers: {
+            cookie: cookie
+        }
+    };
+    const result = await fetch("https://intra.epitech.eu/module/" + scolarYear + "/" + code + "/" + codeInstance + "/" + codeActivity + "/" + codeEvent + "/registered?format=json", opts)
+    if (result.statusCode >= 400)
+        throw "error code : " + result.statusCode;
+    return result.json();
+}
+
+export async function registerInModule(scolarYear, code, codeInstance, cookie) {
+    const opts = {
+        method: 'POST',
+        headers: {
+            cookie: cookie
+        }
+    };
+    const result = await fetch("https://intra.epitech.eu/module/" + scolarYear + "/" + code + "/" + codeInstance + "/register?format=json", opts)
+    if (result.statusCode >= 400)
+        throw "error code : " + result.statusCode;
+    return result.json();
+}
+
+export async function unregisterInModule(scolarYear, code, codeInstance, cookie) {
+    const opts = {
+        method: 'POST',
+        headers: {
+            cookie: cookie
+        }
+    };
+    const result = await fetch("https://intra.epitech.eu/module/" + scolarYear + "/" + code + "/" + codeInstance + "/unregister?format=json", opts)
+    if (result.statusCode >= 400)
+        throw "error code : " + result.statusCode;
+    return result.json();
+}
+
+export async function registerInEvent(scolarYear, code, codeInstance, codeActivity, codeEvent, cookie) {
+    const opts = {
+        method: 'POST',
+        headers: {
+            cookie: cookie
+        }
+    };
+    const result = await fetch("https://intra.epitech.eu/module/" + scolarYear + "/" + code + "/" + codeInstance + "/" + codeActivity + "/" + codeEvent + "/register?format=json", opts)
+    if (result.statusCode >= 400)
+        throw "error code : " + result.statusCode;
+    return result.json();
+}
+
+export async function unregisterInEvent(scolarYear, code, codeInstance, codeActivity, codeEvent, cookie) {
+    const opts = {
+        method: 'POST',
+        headers: {
+            cookie: cookie
+        }
+    };
+    const result = await fetch("https://intra.epitech.eu/module/" + scolarYear + "/" + code + "/" + codeInstance + "/" + codeActivity + "/" + codeEvent + "/unregister?format=json", opts)
+    if (result.statusCode >= 400)
+        throw "error code : " + result.statusCode;
+    return result.json();
+}
+
+export async function destroyProjectGroup(scolarYear, code, codeInstance, codeActivity, cookie) {
+    const opts = {
+        method: 'POST',
+        headers: {
+            cookie: cookie
+        }
+    };
+    const result = await fetch("https://intra.epitech.eu/module/" + scolarYear + "/" + code + "/" + codeInstance + "/" + codeActivity + "/project/destroygroup?format=json", opts)
+    if (result.statusCode >= 400)
+        throw "error code : " + result.statusCode;
+    return result.json();
+}
+
+export async function registerProjectGroup(scolarYear, code, codeInstance, codeActivity, cookie) {
+    const opts = {
+        method: 'POST',
+        headers: {
+            cookie: cookie
+        }
+    };
+    const result = await fetch("https://intra.epitech.eu/module/" + scolarYear + "/" + code + "/" + codeInstance + "/" + codeActivity + "/project/register?format=json", opts)
+    if (result.statusCode >= 400)
+        throw "error code : " + result.statusCode;
+    return result.json();
+}
+
+export async function registerRDV(scolarYear, code, codeInstance, codeActivity, cookie) {
+    const opts = {
+        method: 'POST',
+        headers: {
+            cookie: cookie
+        }
+    };
+    const result = await fetch("https://intra.epitech.eu/module/" + scolarYear + "/" + code + "/" + codeInstance + "/" + codeActivity + "/rdv/register?format=json", opts)
+    if (result.statusCode >= 400)
+        throw "error code : " + result.statusCode;
+    return result.json();
+}
+
+export async function unregisterRDV(scolarYear, code, codeInstance, codeActivity, cookie) {
+    const opts = {
+        method: 'POST',
+        headers: {
+            cookie: cookie
+        }
+    };
+    const result = await fetch("https://intra.epitech.eu/module/" + scolarYear + "/" + code + "/" + codeInstance + "/" + codeActivity + "/rdv/unregister?format=json", opts)
+    if (result.statusCode >= 400)
+        throw "error code : " + result.statusCode;
+    return result.json();
+}
+
+export async function getPlanning(start, end, cookie) {
+    const opts = {
+        headers: {
+            cookie: cookie
+        }
+    };
+    const result = await fetch("https://intra.epitech.eu/planning/load?format=json&start=" + start + "&end=" + end, opts)
+    if (result.statusCode >= 400)
+        throw "error code : " + result.statusCode;
+    return result.json();
 }
