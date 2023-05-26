53fa610cd46eba48fa7defe9b0dcc841c8172af5
KeziahImer <keziah.imer@epitech.eu>
2023-05-26 19:05:21 +0200

    [Feat] (model) : add AllFlags

--- app/model/profile.js
+++ app/model/profile.js
@@ -25,3 +25,17 @@ export async function getAllNoteInProfile(email, cookie) {
         throw "error code : " + result.statusCode;
     return result.json();
 }
+
+export async function getAllFlagsInProfile(email, cookie) {
+    const opts = {
+        headers: {
+            cookie: cookie,
+            "Content-Type": "application/json",
+            accept: "application/json"
+        }
+    };
+    const result = await fetch("https://intra.epitech.eu/user/" + email + "/flags/?format=json", opts)
+    if (result.statusCode >= 400)
+        throw "error code : " + result.statusCode;
+    return result.json();
+}
