72bc7f0e12c6862b3223d7d3e88c6cbdd0f947dc
KeziahImer <keziah.imer@epitech.eu>
2023-05-26 19:06:05 +0200

    [Feat] (model) : add AllDocuments

--- app/model/profile.js
+++ app/model/profile.js
@@ -67,3 +67,17 @@ export async function getAllMissedInProfile(email, cookie) {
         throw "error code : " + result.statusCode;
     return result.json();
 }
+
+export async function getAllDocumentsInProfile(email, cookie) {
+    const opts = {
+        headers: {
+            cookie: cookie,
+            "Content-Type": "application/json",
+            accept: "application/json"
+        }
+    };
+    const result = await fetch("https://intra.epitech.eu/user/" + email + "/document/?format=json", opts)
+    if (result.statusCode >= 400)
+        throw "error code : " + result.statusCode;
+    return result.json();
+}
