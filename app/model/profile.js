c0fbe50a8d454386ed5c582c88d1243dbce85a1c
KeziahImer <keziah.imer@epitech.eu>
2023-05-26 19:05:04 +0200

    [Feat] (model) : add AllNote

--- app/model/profile.js
+++ app/model/profile.js
@@ -11,3 +11,17 @@ export async function getProfile(cookie) {
         throw "error code : " + result.statusCode;
     return result.json();
 }
+
+export async function getAllNoteInProfile(email, cookie) {
+    const opts = {
+        headers: {
+            cookie: cookie,
+            "Content-Type": "application/json",
+            accept: "application/json"
+        }
+    };
+    const result = await fetch("https://intra.epitech.eu/user/" + email + "/notes/?format=json", opts)
+    if (result.statusCode >= 400)
+        throw "error code : " + result.statusCode;
+    return result.json();
+}
