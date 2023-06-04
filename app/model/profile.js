d8061ba22e1ec5bc710216f2c88ad96d358146d5
KeziahImer <keziah.imer@epitech.eu>
2023-06-04 16:25:04 +0200

    [Feat] (model) : edit profile to add netsoul

--- app/model/profile.js
+++ app/model/profile.js
@@ -81,3 +81,17 @@ export async function getAllDocumentsInProfile(email, cookie) {
         throw "error code : " + result.statusCode;
     return result.json();
 }
+
+export async function getNetsoulInProfile(email, cookie) {
+    const opts = {
+        headers: {
+            cookie: cookie,
+            "Content-Type": "application/json",
+            accept: "application/json"
+        }
+    };
+    const result = await fetch("https://intra.epitech.eu/user/" + email + "/netsoul/?format=json", opts)
+    if (result.statusCode >= 400)
+        throw "error code : " + result.statusCode;
+    return result.json();
+}
