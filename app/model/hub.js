ac8766537edde729aca6075d891c9c091ad9be26
SimonVermeulen <simon.vermeulen@epitech.eu>
2023-06-04 22:04:15 +0200

    [Fix] (Model) : creation of the model for the hub

--- app/model/hub.js
+++ app/model/hub.js
@@ -1,7 +1,7 @@
 import {getAllModules, getModuleInformation} from "./module.js";
 
 export async function getAllHubModules(cookie) {
-    const modules = getAllModules(cookie);
+    const modules = await getAllModules(cookie);
     let array = [];
 
     for (let i = 0; i < modules.length; i++) {
@@ -14,19 +14,38 @@ export async function getAllHubModules(cookie) {
 }
 
 export async function getAllActivityHub(cookie) {
-    const modules = getAllHubModules(cookie);
+    const modules = await getAllHubModules(cookie);
     let activities = [];
 
     for (let i = 0; i < modules.length; i++) {
-        const info = getModuleInformation(modules[i].scolaryear, modules[i].code, modules[i].codeinstance, cookie);
+        const info = await getModuleInformation(modules[i].scolaryear, modules[i].code, modules[i].codeinstance, cookie);
 
-        activities.concat(info.activites);
+        activities = [...activities, ...info.activites];
+    }
+    return activities;
+}
+
+export async function getAllActivityHubByYear(year, cookie) {
+    const modules = await getAllHubModules(cookie);
+    let activities = [];
+
+    for (let i = 0; i < modules.length; i++) {
+        if (year !== modules[i].scolaryear)
+            continue;
+        const info = await getModuleInformation(modules[i].scolaryear, modules[i].code, modules[i].codeinstance, cookie);
+
+        for (let j = 0; j < info.activites.length; j++) {
+            info.activites[j]["scholarYear"] = modules[i].scolaryear;
+            info.activites[j]["codeModule"] = modules[i].code;
+            info.activites[j]["codeInstance"] = modules[i].codeinstance;
+        }
+        activities = [...activities, ...info.activites];
     }
     return activities;
 }
 
 export async function getAllJamModules(cookie) {
-    const modules = getAllModules(cookie);
+    const modules = await getAllModules(cookie);
     let array = [];
 
     for (let i = 0; i < modules.length; i++) {
