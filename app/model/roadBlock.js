626f692611398ce30e3b80d0bb895cade4af8bb1
manal67 <manal.bahri@epitech.eu>
2023-06-05 21:55:54 +0200

    [Feat] (model) : edit roadblock

--- app/model/roadBlock.js
+++ app/model/roadBlock.js
@@ -1,28 +1,32 @@
-import {getAllModules} from "./module.js";
+import {getAllModules, getModuleInformation} from "./module.js";
 
 export async function getAllRoadBlock(cookie) {
-    const modules = getAllModules(cookie);
+    const modules = await getAllModules(cookie);
     let array = [];
 
     for (let i = 0; i < modules.length; i++) {
         let title = modules[i].title;
 
-        if (title.includes("Roadblock"))
-            array.push(modules[i])
+        if (title.includes("Roadblock")) {
+            let info = await getModuleInformation(modules[i].scolaryear, modules[i].code, modules[i].codeinstance, cookie);
+            array.push(info)
+        }
     }
     return array;
 }
 
-export async function getAllRoadBlockByYear(cookie, year) {
-    const modules = get(cookie);
+export async function getAllRoadBlockByYear(year, cookie) {
+    const modules = await getAllModules(cookie);
     let array = [];
 
     for (let i = 0; i < modules.length; i++) {
         let title = modules[i].title;
-        let scolarYear = modules[i].scolaryear;
+        let scolarYear = modules[i].scolaryear.toString();
 
-        if (title.includes("Roadblock") && scolarYear === year)
-            array.push(modules[i])
+        if (title.includes("Roadblock") && scolarYear === year) {
+            let info = await getModuleInformation(modules[i].scolaryear, modules[i].code, modules[i].codeinstance, cookie);
+            array.push(info)
+        }
     }
     return array;
 }
