c79cb9dc96f949bf1310744e3fc728e9e4acb458
manal67 <manal.bahri@epitech.eu>
2023-06-05 21:55:42 +0200

    [Feat] (controller) : edit roadblock

---
+++ app/controller/roadblock.js
@@ -0,0 +1,86 @@
+import {getAllRoadBlock, getAllRoadBlockByYear} from "../model/roadBlock.js";
+import {getAllModules} from "../model/module.js";
+
+
+function getAllModuleRequired(description) {
+    let modules = [];
+    let lines = description.split('\n');
+    lines.splice(0, 3);
+    lines.forEach((value) => {
+        value = value.trim();
+        if (value === '')
+            return;
+        modules.push(value);
+    })
+    return modules;
+}
+
+function getCreditRequired(description) {
+    let credits = description.split("at least ")[1].split(" credits")[0];
+    return Number.parseInt(credits);
+}
+
+export default function index(app) {
+    app.get('/roadblocks', async (request, response) => {
+        let cookies = request.headers.cookie;
+
+        let roadblocks = await getAllRoadBlock(cookies);
+        let results = [];
+        let modules = await getAllModules(cookies);
+
+        roadblocks.forEach((roadblock) => {
+            let result = {
+                scholarYear: roadblock.scolaryear,
+                codeModule: roadblock.codemodule,
+                codeInstance: roadblock.codeinstance,
+                title: roadblock.title
+            }
+            let modulesInfo = getAllModuleRequired(roadblock.description);
+            let modulesRoadBlock = [];
+            modules.forEach((module) => {
+                for (let i = 0; i < modulesInfo.length; i++) {
+                    if (!modulesInfo[i].includes(module.code))
+                        continue;
+                    modulesRoadBlock.push(module);
+                }
+            })
+            let creditsRequired = getCreditRequired(roadblock.description);
+            result["modulesRequired"] = modulesRoadBlock;
+            result["creditsRequired"] = creditsRequired;
+            results.push(result);
+        })
+        return response.status(200).json(results);
+    })
+    app.get('/roadblocks/:year', async (request, response) => {
+        let cookies = request.headers.cookie;
+        let year = request.params.year;
+
+        let roadblocks = await getAllRoadBlockByYear(year, cookies);
+        let results = [];
+        let modules = await getAllModules(cookies);
+
+        console.log(roadblocks)
+        roadblocks.forEach((roadblock) => {
+            let result = {
+                scholarYear: roadblock.scolaryear,
+                codeModule: roadblock.codemodule,
+                codeInstance: roadblock.codeinstance,
+                title: roadblock.title
+            }
+            let modulesInfo = getAllModuleRequired(roadblock.description);
+            let modulesRoadBlock = [];
+            modules.forEach((module) => {
+                for (let i = 0; i < modulesInfo.length; i++) {
+                    if (!modulesInfo[i].includes(module.code))
+                        continue;
+                    modulesRoadBlock.push(module);
+                }
+            })
+            let creditsRequired = getCreditRequired(roadblock.description);
+            result["modulesRequired"] = modulesRoadBlock;
+            result["creditsRequired"] = creditsRequired;
+            results.push(result);
+        })
+        return response.status(200).json(results);
+    })
+}
